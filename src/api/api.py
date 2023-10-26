a07708d0a404e866957c70e092f24ddbcf83dfd0
Neil Desplats <neil.desplats@grenoble-inp.fr>
2023-10-26 09:00:22 +0200

    feat: OMG je suis utile en IA

---
+++ src/api/api.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class Api:
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    def __init__(self):
+        ...
+
+    def fetch(self):
+        params = {
+            "latitude": "45.18857814633678",
+            "longitude": "5.718267792253707",
+            "hourly": "relativehumidity_2m,dewpoint_2m,cloudcover_mid,windspeed_80m,winddirection_80m,temperature_80m",
+            "daily": "temperature_2m_max,temperature_2m_min",
+            "timezone": "Europe/Berlin",
+            "forecast_days": "1"
+        }
+        r = requests.get(self.url, params)
+        return r.json()
+
+
+if __name__ == "__main__":
+    api = Api()
+    data = api.fetch()
+    print(data)
